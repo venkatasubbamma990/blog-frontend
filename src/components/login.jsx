d7a045ac143bbed79130f6442a4f384ca77a904e
venkatasubbamma990 <venkatasubbamma990@email.com>
2022-12-28 16:32:42 +0530

    Initialize project using Create React App

---
+++ src/components/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+let Login = () =>{
+    return(
+        <div>
+            <form action="">
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
+export default Login
